185ad86b1130107695193b9e05d35672126ec7e1
Colin Sullivan <colin@colin-sullivan.net>
2015-10-31 23:30:35 -0700

    supercollider api talks to node

--- tawNode/index.js
+++ tawNode/index.js
@@ -2,6 +2,47 @@ var pixel = require("node-pixel");
 
 var five = require("johnny-five");
 
+var supercolliderjs = require("supercolliderjs");
+
+supercolliderjs.resolveOptions(null, {
+  debug: true,
+  stdin: false
+}).then(function(options) {
+
+  var lang = new supercolliderjs.sclang(options);
+  /*lang.boot().then(function () {
+    return lang.initInterpreter();
+  }).then(function () {
+    lang.interpret("API.mountDuplexOSC").then(function (result) {
+      console.log("result");
+      console.log(result);
+    });
+  });*/
+  /*.then(function () {
+  });*/
+  var api = new supercolliderjs.scapi(options.host, options.langPort);
+  api.log.dbug(options);
+
+  console.log("connecting...");
+  api.connect();
+  console.log("calling...");
+  api.call(0, "taw.hello", []).then(function (resp) {
+    console.log("resp");
+    console.log(resp);
+  });
+
+  /*var Server = supercolliderjs.scsynth;
+  var s = new Server(options);
+  s.boot();*/
+
+  //console.log("resolveOptions done");
+  //console.log("options");
+  //console.log(options);
+
+});
+
+
+
 var board = new five.Board({
   repl: false
 });
@@ -43,3 +84,5 @@ board.on("ready", function () {
     }, 30);
   });
 });
+
+
--- tawSuperCollider/main.sc
+++ tawSuperCollider/main.sc
@@ -1,7 +1,15 @@
 ({
 
-  SerialPort.listDevices();
+  //API.mountDuplexOSC;
+
+  //API.loadAll.allPaths
+
 
 }.value());
 
 Quarks.gui();
+
+Quarks.install("taw");
+
+API.loadAll.allPaths.do(_.postln);
+
