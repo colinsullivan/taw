dabbd9a31a9685f53763f8fb2a03bfc9368b7ddf
eclefley <eclefley@gmail.com>
2018-02-27 20:50:40 -0800

    ok looks legit

--- src/testFadecandyColor.js
+++ src/testFadecandyColor.js
@@ -5,11 +5,29 @@ var MAX_PIXELS = 64 * 8;
 var client = new OPC("localhost", 7890),
   i;
 
-// set all pixels to the given color
-let colorRGB = [0, 0, 0];
-// or specify color in hsv
-colorRGB = OPC.hsv(0.5, 1.0, 1.0);
-for (i = MAX_PIXELS - 1; i >= 0; i--) {
-  client.setPixel(i, colorRGB[0], colorRGB[1], colorRGB[2]);
-}
-client.writePixels();
+var draw = function () {
+  
+  
+  
+  
+  
+  // set all pixels to the given color [0 - 255]
+  let colorRGB = [0, 0, 0];
+  // or specify color in hsv [0 - 1.0]
+  colorRGB = OPC.hsv(0.1, 1.0, 1.0);
+  
+  
+  
+  
+  
+  
+  for (i = MAX_PIXELS - 1; i >= 0; i--) {
+    client.setPixel(i, colorRGB[0], colorRGB[1], colorRGB[2]);
+  }
+  
+  client.writePixels();
+
+  setTimeout(draw, 30);
+};
+
+setTimeout(draw, 30);
